60b9e632fa70da761394641d74baebb64c87e5b8
Elliot Hillary <ehillary@integradev.com.au>
2022-12-14 11:07:18 +1100

    Override sonarlint.core.commons.Language to include Delphi

--- server/src/main/java/au/com/integradev/delphilint/App.java
+++ server/src/main/java/au/com/integradev/delphilint/App.java
@@ -1,16 +1,14 @@
 package au.com.integradev.delphilint;
 
-import java.io.Console;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
-import org.sonar.api.utils.log.Logger;
-import org.sonar.api.utils.log.Loggers;
 import org.sonarsource.sonarlint.core.analysis.api.AnalysisConfiguration;
 import org.sonarsource.sonarlint.core.analysis.api.AnalysisEngineConfiguration;
 import org.sonarsource.sonarlint.core.analysis.container.global.GlobalAnalysisContainer;
 import org.sonarsource.sonarlint.core.analysis.container.module.ModuleContainer;
+import org.sonarsource.sonarlint.core.commons.Language;
 import org.sonarsource.sonarlint.core.commons.progress.ProgressMonitor;
 import org.sonarsource.sonarlint.core.plugin.commons.PluginInstancesRepository;
 
@@ -23,6 +21,11 @@ public class App {
         var analysisGlobalConfig = AnalysisEngineConfiguration.builder()
             .setWorkDir(Path.of("{PATH REMOVED}"))
             .build();
+
+        // Debugging
+        for(var value : Language.values()) {
+            System.out.println(value);
+        }
 
         var pluginConfig =
             new PluginInstancesRepository.Configuration(
