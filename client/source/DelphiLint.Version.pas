e5e5b481cc5d29f8f1c1728d38995e0cea6a1344
Elliot Hillary <ehillary@integradev.com.au>
2023-07-28 15:32:50 +1000

    Comply with semver for dev builds with +dev.sha format

--- client/source/DelphiLint.Version.pas
+++ client/source/DelphiLint.Version.pas
@@ -26,7 +26,7 @@ function DelphiLintVersion: string;
 begin
   Result := Format('%d.%d.%d', [C_DlMajorVersion, C_DlMinorVersion, C_DlPatchVersion]);
   if C_DlIsDevVersion then begin
-    Result := Format('%s.%s', [Result, C_DlCommit]);
+    Result := Format('%s+dev.%s', [Result, C_DlCommit]);
   end;
 end;
 
